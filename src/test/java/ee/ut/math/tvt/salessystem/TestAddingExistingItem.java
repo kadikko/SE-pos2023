4efe1fa0f37d3de0795290642b0714846d3bcb3b
kadikko <kadi.koppelmann@ut.ee>
2023-11-06 11:37:44 +0200

    removing previous test modules under cli and gui, temporarily disabling tests to check if pipeline build works without them.

--- src/test/java/ee/ut/math/tvt/salessystem/TestAddingExistingItem.java
+++ src/test/java/ee/ut/math/tvt/salessystem/TestAddingExistingItem.java
@@ -5,6 +5,7 @@ import ee.ut.math.tvt.salessystem.dao.InMemorySalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -24,6 +25,7 @@ public class TestAddingExistingItem {
         mockDao = mock(InMemorySalesSystemDAO.class);
     }
 
+    @Disabled
     @Test
     @DisplayName("Should show if adding an exiting item increases the stock quantity and if the saveStockItem method of the DAO is not called")
     public void testAddingExistingItem() {
--- src/test/java/ee/ut/math/tvt/salessystem/TestAddingNewItem.java
+++ src/test/java/ee/ut/math/tvt/salessystem/TestAddingNewItem.java
@@ -1,10 +1,10 @@
 package ee.ut.math.tvt.salessystem;
 
-
 import ee.ut.math.tvt.salessystem.dao.InMemorySalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +19,7 @@ public class TestAddingNewItem {
         dao = new InMemorySalesSystemDAO();
     }
 
+    @Disabled
     @Test
     @DisplayName("Should show if a new item is saved through the DAO")
     public void testAddingNewItem() {
