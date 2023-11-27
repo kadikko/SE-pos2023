3ff16fd6a5505b69125bd5ed6c58dcfd89e3d76d
KarlTomasson <karltomasson3@gmail.com>
2023-11-28 01:27:10 +0200

    Added tests, close #69, #70

---
+++ src/test/java/ee/ut/math/tvt/salessystem/TestSubmittingCurrentOrderSavesCorrectTime.java
@@ -0,0 +1,40 @@
+package ee.ut.math.tvt.salessystem;
+
+import ee.ut.math.tvt.salessystem.dao.InMemorySalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dataobjects.PreviousCart;
+import ee.ut.math.tvt.salessystem.dataobjects.SoldItem;
+import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
+import ee.ut.math.tvt.salessystem.logic.ShoppingCart;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class TestSubmittingCurrentOrderSavesCorrectTime {
+    private SalesSystemDAO dao;
+    private ShoppingCart shoppingCart;
+
+
+    @BeforeEach
+    void setup() {
+        dao = new InMemorySalesSystemDAO();
+        shoppingCart = new ShoppingCart(dao);
+    }
+
+    @Test
+    @DisplayName("Should test if submitting a purchase makes a previousCart have accurate dates")
+    public void testSubmittingCurrentOrderSavesCorrectTime() {
+        LocalTime aeg = LocalTime.now();
+        shoppingCart.submitCurrentPurchase();
+        PreviousCart cart = dao.findPreviousCartList().get(dao.findPreviousCartList().size()-1);
+        assertEquals(aeg.getSecond(), cart.getTime().getSecond());
+
+    }
+}
